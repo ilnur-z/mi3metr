548141be097439a4016eae0daa445651bd2aae6a
ilnur <ilnur@ARM315-17.npovs.icl.kazan.ru>
2024-05-07 12:42:47 +0300

    Добавлено голосование по клику на картинке

--- mi3metr/src/main/resources/static/voteStage.js
+++ mi3metr/src/main/resources/static/voteStage.js
@@ -1,6 +1,10 @@
 	$(document).ready(function(){
-	   $('a').on("click", function(){
+	   $('.candidateLink').on("click", function(){
 	       $(this).prev().prop('checked', true);
+	       $('#voteForm').submit();
+	   });
+	   $('.candidateImage').on("click", function(){
+	       $(this).next().prop('checked', true);
 	       $('#voteForm').submit();
 	   });
 	});
